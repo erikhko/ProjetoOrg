45cfdb49c60ef92c7048a737de64962f5e2e35b2
Erik Ko <97708892+erikhko@users.noreply.github.com>
2024-03-15 01:32:20 -0300

    Código fonte do layout inicial do projeto

---
+++ orgs/app/src/main/java/br/com/fiap/orgs/MainActivity.kt
@@ -0,0 +1,32 @@
+package br.com.fiap.orgs
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.orgs.ui.theme.OrgsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Binding: processo de vinculação do código fonte com o arquivo de layout
+
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.setText("Cesta de Frutas")
+
+        val descricao = findViewById<TextView>(R.id.descricao)
+        nome.setText("Laranja, Uva e Maçã")
+
+        val valor = findViewById<TextView>(R.id.valor)
+        nome.setText("Laranja, Uva e Maçã")
+
+
+    }
+}
